9c3ebea1ba8c24e0eebb377d932f015e83d3d8a3
Tanmay Gautam <69148875+Tanmay-Gautam@users.noreply.github.com>
2023-06-29 22:56:21 +0530

    localstorage-testing

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ addBtn.addEventListener("click", addTodo);
     "Tuesday 😃",
     "Wednesday 😃",
     "Thursday 😃",
-    " Friday 👍✌️",
+    "Friday 👍✌️",
     "Saturday ​🏠​🎊​🎶​",
   ];
   const randomWords = ["Enjoy your ", "Happy "];
@@ -27,58 +27,100 @@ addBtn.addEventListener("click", addTodo);
 
 // Functions
 
-function addTodo(e) {
-  // Prevent natural behaviour
-  try {
-    e.preventDefault();
-  } catch (err) {
-    err = null;
-  }
-
+function addTodo() {
   if (todoInput.value !== "") {
     // Create list
     const newTodo = document.createElement("li");
-    // newTodo.innerHTML += '<span><input class="checkbox" type="checkbox">'+'<p>'+todoInput.value+'</p>'+'</span>';
     newTodo.innerHTML += '<span><i class="checkbox far fa-circle"></i>'+'<p>'+todoInput.value+'</p>'+'</span>';
     newTodo.innerHTML += '<i class="btn fas fa-times remove-todo-btn" aria-hidden="true"></i>';
     newTodo.classList.add("todo-item", "container");
 
-    newTodo.addEventListener("click", e => {
-
+    newTodo.addEventListener("click", (e) => {
       // add success class to todo
       if (!e.target.classList.contains("remove-todo-btn")) {
-        // console.log(e);
         newTodo.classList.toggle("completed");
 
         // changing the check icon
-        if(e.target.closest("li").querySelector(".checkbox").classList.contains("far")){
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("fas"))
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("far"))
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("fa-check"))
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("fa-circle"))
-        }
-        else{
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("fas"))
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("far"))
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("fa-check"))
-          console.log(e.target.closest("li").querySelector(".checkbox").classList.toggle("fa-circle"))
-        }
-        // if(e.target.closest("li")
+        const checkboxIcon = e.target.closest("li").querySelector(".checkbox");
+        checkboxIcon.classList.toggle("fas");
+        checkboxIcon.classList.toggle("far");
+        checkboxIcon.classList.toggle("fa-check");
+        checkboxIcon.classList.toggle("fa-circle");
       }
-
       // remove todo after animation
-      else if (e.target.classList.contains("remove-todo-btn")){
+      else if (e.target.classList.contains("remove-todo-btn")) {
         setTimeout(() => {
           e.target.parentElement.remove();
+          updateLocalStorage();
         }, 990);
         e.target.parentElement.classList.remove("completed");
         e.target.parentElement.classList.add("remove-todo");
       }
     });
+
     todoList.appendChild(newTodo);
     todoInput.value = "";
+
+    updateLocalStorage();
   }
 }
+
+function updateLocalStorage() {
+  const todos = Array.from(todoList.getElementsByTagName("li")).map((todo) => ({
+    text: todo.querySelector("p").innerText,
+    completed: todo.classList.contains("completed"),
+  }));
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function restoreTodos() {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  if (todos) {
+    todos.forEach((todo) => {
+      const newTodo = document.createElement("li");
+      newTodo.innerHTML += '<span><i class="checkbox far fa-circle"></i>'+'<p>'+todo.text+'</p>'+'</span>';
+      newTodo.innerHTML += '<i class="btn fas fa-times remove-todo-btn" aria-hidden="true"></i>';
+      newTodo.classList.add("todo-item", "container");
+
+      if (todo.completed) {
+        newTodo.classList.add("completed");
+        const checkboxIcon = newTodo.querySelector(".checkbox");
+        checkboxIcon.classList.toggle("fas");
+        checkboxIcon.classList.toggle("far");
+        checkboxIcon.classList.toggle("fa-check");
+        checkboxIcon.classList.toggle("fa-circle");
+      }
+
+      newTodo.addEventListener("click", (e) => {
+        // add success class to todo
+        if (!e.target.classList.contains("remove-todo-btn")) {
+          newTodo.classList.toggle("completed");
+
+          // changing the check icon
+          const checkboxIcon = e.target.closest("li").querySelector(".checkbox");
+          checkboxIcon.classList.toggle("fas");
+          checkboxIcon.classList.toggle("far");
+          checkboxIcon.classList.toggle("fa-check");
+          checkboxIcon.classList.toggle("fa-circle");
+        }
+        // remove todo after animation
+        else if (e.target.classList.contains("remove-todo-btn")) {
+          setTimeout(() => {
+            e.target.parentElement.remove();
+            updateLocalStorage();
+          }, 990);
+          e.target.parentElement.classList.remove("completed");
+          e.target.parentElement.classList.add("remove-todo");
+        }
+      });
+
+      todoList.appendChild(newTodo);
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", restoreTodos);
 
 todoInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
